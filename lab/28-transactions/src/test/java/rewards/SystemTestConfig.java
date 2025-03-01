d3485d3b3cc46100d8219fd09e74887ddedbec1f
Kacper <s27427@pjwstk.edu.pl>
2025-03-01 18:16:03 +0100

    Lab 28-transactional done

--- lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
+++ lab/28-transactions/src/test/java/rewards/SystemTestConfig.java
@@ -5,9 +5,11 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import config.RewardsConfig;
+import org.springframework.transaction.PlatformTransactionManager;
 
 
 @Configuration
@@ -28,7 +30,11 @@ public class SystemTestConfig {
 			.build();
 	}	
 	
-	
+	@Bean
+	public PlatformTransactionManager transactionManager(){
+		return new DataSourceTransactionManager(dataSource());
+	}
+
 	//	TODO-02: Define a bean named 'transactionManager' that configures a
 	//           DataSourceTransactionManager.
 	//           How does it know which dataSource to manage?
