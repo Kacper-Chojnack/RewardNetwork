8f4672afb2ec84a0c6a881c808ad6c239e3a4fd3
Kacper <s27427@pjwstk.edu.pl>
2025-03-01 15:29:34 +0100

    Lab 26-jdbc done

--- lab/26-jdbc/src/main/java/config/RewardsConfig.java
+++ lab/26-jdbc/src/main/java/config/RewardsConfig.java
@@ -1,11 +1,9 @@
 package config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -15,36 +13,42 @@ import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.JdbcRewardRepository;
 import rewards.internal.reward.RewardRepository;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class RewardsConfig {
 
 	@Autowired
 	DataSource dataSource;
-		
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource);
+	}
+
 	@Bean
 	public RewardNetwork rewardNetwork(){
 		return new RewardNetworkImpl(
-			accountRepository(), 
-			restaurantRepository(), 
-			rewardRepository());
+				accountRepository(),
+				restaurantRepository(),
+				rewardRepository());
 	}
-	
+
 	@Bean
 	public AccountRepository accountRepository(){
-		JdbcAccountRepository repository = new JdbcAccountRepository(dataSource);
+		JdbcAccountRepository repository = new JdbcAccountRepository(jdbcTemplate());
 		return repository;
 	}
-	
+
 	@Bean
 	public RestaurantRepository restaurantRepository(){
-		JdbcRestaurantRepository repository = new JdbcRestaurantRepository(dataSource);
-		return repository;
+        return new JdbcRestaurantRepository(jdbcTemplate());
 	}
-	
+
 	@Bean
 	public RewardRepository rewardRepository(){
-		JdbcRewardRepository repository = new JdbcRewardRepository(dataSource);
+		JdbcRewardRepository repository = new JdbcRewardRepository(jdbcTemplate());
 		return repository;
 	}
-	
+
 }
